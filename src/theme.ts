fef2338578690a0af64c2c382a04a6a762bfa7b4
Ethan Hosier <ehosier@palantir.com>
2025-04-11 10:12:23 +0100

    feat: change color theme

--- src/theme.ts
+++ src/theme.ts
@@ -2,16 +2,16 @@ import { createTheme, MantineColorsTuple } from "@mantine/core";
 import "./styles/fonts.css";
 
 const myColor: MantineColorsTuple = [
-  "#fcf1fe",
-  "#ede2ef",
-  "#d4c4d7",
-  "#bba5bf",
-  "#a589ab",
-  "#98789f",
-  "#926f99",
-  "#7f5e86",
-  "#725378",
-  "#64466b",
+  "#fff4e1",
+  "#ffe8cc",
+  "#fed09b",
+  "#fdb766",
+  "#fca13a",
+  "#fc931d",
+  "#fc8c0c",
+  "#e17800",
+  "#c86a00",
+  "#af5a00"
 ];
 
 export const theme = createTheme({
